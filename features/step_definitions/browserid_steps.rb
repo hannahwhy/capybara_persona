97a3aae284dda608b2f2b57844b32458a1845633
David Yip <yipdw@member.fsf.org>
2013-02-09 17:30:14 -0600

    Move steps concerned with this library to a separate file.

---
+++ features/step_definitions/browserid_steps.rb
@@ -0,0 +1,19 @@
+When /^I provide the email address "(.*?)"$/ do |email|
+  persona.set_email(email)
+end
+
+When /^I provide the password "(.*?)"$/ do |password|
+  persona.set_password(password)
+end
+
+When /^I submit my credentials$/ do
+  persona.submit_credentials
+end
+
+Then /^I am prompted to create a password$/ do
+  persona.state.should == :create_password
+end
+
+Then /^the Persona window appears$/ do
+  persona.should have_visible_window
+end
--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -13,26 +13,6 @@ When /^I click "(.*?)"$/ do |name|
   click_button 'Sign in'
 end
 
-When /^I provide the email address "(.*?)"$/ do |email|
-  persona.set_email(email)
-end
-
-When /^I provide the password "(.*?)"$/ do |password|
-  persona.set_password(password)
-end
-
-When /^I submit my credentials$/ do
-  persona.submit_credentials
-end
-
-Then /^I am prompted to create a password$/ do
-  persona.state.should == :create_password
-end
-
-Then /^the Persona window appears$/ do
-  persona.should have_visible_window
-end
-
 Then /^I am signed in$/ do
   page.should have_content('Signed in')
 end
