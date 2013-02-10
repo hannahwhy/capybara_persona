4265e3b1573852fe9476e7c9242121a17dfc60c1
David Yip <yipdw@member.fsf.org>
2013-02-10 16:20:19 -0600

    More Travis diagnosis: show me what's going on.

--- features/support/env.rb
+++ features/support/env.rb
@@ -5,6 +5,7 @@ require 'capybara/persona'
 require File.expand_path('../app/app', __FILE__)
 
 Capybara.app = App
+Capybara.default_wait_time = 60
 Capybara.default_selector = :css
 
 class TestWorld
@@ -20,6 +21,34 @@ Before do
            end
 
   Capybara.current_driver = driver
+end
+
+$STEP = 0
+
+require 'fileutils'
+require 'aws/s3'
+include FileUtils
+
+AfterStep do
+  mkdir_p '/tmp/shots'
+  `import -window root /tmp/shots/#{$STEP}.png`
+  $STEP += 1
+end
+
+at_exit do
+  Dir.chdir('/tmp') do
+    `tar zcvf shots.tar.gz shots`
+  end
+
+  AWS::S3::Base.establish_connection!(
+    :access_key_id => ENV['ACCESS_KEY_ID'],
+    :secret_access_key => ENV['SECRET_ACCESS_KEY']
+  )
+
+  include AWS::S3
+
+  fn = "shots-#{Time.now.to_i}.tar.gz"
+  AWS::S3::S3Object.store(fn, open('/tmp/shots.tar.gz'), 'uberjunk')
 end
 
 After do
