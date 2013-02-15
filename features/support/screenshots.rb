b28986f1431bd7ed3cfa221414f5345487259999
David Yip <yipdw@member.fsf.org>
2013-02-15 03:56:07 -0600

    Eliminate step counter; use timestamps instead.

--- features/support/screenshots.rb
+++ features/support/screenshots.rb
@@ -2,13 +2,13 @@ require 'shellwords'
 
 if ENV['CAPTURE_DIR']
   Before do
-    @step_counter = 0
     mkdir_p ENV['CAPTURE_DIR']
   end
 
   AfterStep do |scenario|
     cdir = ENV['CAPTURE_DIR']
-    fn = Shellwords.escape("#{cdir}/#{scenario.title}-#{@step_counter}.png")
+    now = Time.now.to_f
+    fn = Shellwords.escape("#{cdir}/#{scenario.title}-#{now}.png")
     `import -window root #{fn}`
   end
 end
